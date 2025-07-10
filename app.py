1c21997fe67dffab91bc16e3250a60256920bd3b
jasnam1234 <jasnamjeetk@gmail.com>
2025-07-10 15:08:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,3 +17,25 @@ try:
 except Exception as e:
   st.error(f"Failed to load data: {e}")
   
+a = st.text_input("Enter a")
+b = st.text_input("Enter b")
+c = st.text_input("Enter c")
+
+if st.button("Upload to GitHub"):
+  df = pd.DataFrame([("a":a, "b":b, "c":c)])
+  csv = df.to_csv(index=False)
+  content = base64.b64encode(csv.encode()).decode()
+
+url = "https://api.github.com/repos/jasnam1234/abcd/contents/data2.csv"
+headers = ("Authorization":f"token {st.secrets['github']['token']}")
+
+payload = {
+  "message": "Add data.csv",
+  "content": content,
+  "branch": "main"
+}
+r = requests.put(url,headers=headers,json=payload)
+if r.status_code in [200,201]:
+  st.success("Data Uploaded!")
+else:
+  st.error("Failed to upload")

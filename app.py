326ca26b9cfdcaa2055abed076dccd9aeda9b0e0
jasnam1234 <jasnamjeetk@gmail.com>
2025-07-10 15:22:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ b = st.text_input("Enter b")
 c = st.text_input("Enter c")
 
 if st.button("Upload to GitHub"):
-  df = pd.DataFrame([("a":a, "b":b, "c":c)])
+  df = pd.DataFrame([{"a":a, "b":b, "c":c}])
   csv = df.to_csv(index=False)
   content = base64.b64encode(csv.encode()).decode()
 
